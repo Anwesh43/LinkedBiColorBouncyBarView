de641148e787f245d7e0f841bf01385829d5b4a7
Anwesh43 <anweshthecool0@gmail.com>
2019-12-23 13:32:28 +0530

    adding BCBBNode class

--- bicolorbouncybarview/src/main/java/com/anwesh/uiprojects/bicolorbouncybarview/BiColorBouncyBarView.kt
+++ bicolorbouncybarview/src/main/java/com/anwesh/uiprojects/bicolorbouncybarview/BiColorBouncyBarView.kt
@@ -26,3 +26,25 @@ fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
 fun Float.cosify() : Float = 1f - Math.sin(Math.PI / 2 + (this * Math.PI * 0.5f)).toFloat()
+
+fun Canvas.drawBiColorBouncyBar(scale : Float, size : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, 2)
+    val sf2 : Float = sf.divideScale(1, 2)
+    val sf3 : Float = scale.divideScale(3, 4).cosify()
+    paint.color = foreColor1
+    drawRect(RectF(-size + 2 * size * sf2, -size, -size + 2 * size * sf2, size), paint)
+    paint.color = foreColor2
+    drawRect(RectF(size - 2 * size * sf3, -size, size, size), paint)
+}
+
+fun Canvas.drawBCBBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawBiColorBouncyBar(scale, size, paint)
+    restore()
+}

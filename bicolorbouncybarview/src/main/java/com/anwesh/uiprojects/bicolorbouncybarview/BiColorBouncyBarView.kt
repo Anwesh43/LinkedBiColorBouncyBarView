c39c69a48b8d233cc76d90c628a25ad68038081d
Anwesh43 <anweshthecool0@gmail.com>
2019-12-23 13:16:45 +0530

    adding Constants

--- bicolorbouncybarview/src/main/java/com/anwesh/uiprojects/bicolorbouncybarview/BiColorBouncyBarView.kt
+++ bicolorbouncybarview/src/main/java/com/anwesh/uiprojects/bicolorbouncybarview/BiColorBouncyBarView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.bicolorbouncybarview
 /**
  * Created by anweshmishra on 23/12/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val scGap : Float = 0.02f
+val delay : Long = 30
+val foreColor1 : Int = Color.parseColor("#9C27B0")
+val foreColor2 : Int = Color.parseColor("#00695C")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val sizeFactor : Float = 2.9f

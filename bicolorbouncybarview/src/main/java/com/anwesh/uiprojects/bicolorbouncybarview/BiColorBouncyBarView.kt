0663783308d7d5df8942593eb43ba39d6ec9ef82
Anwesh43 <anweshthecool0@gmail.com>
2019-12-23 13:25:23 +0530

    adding Int and FLoat extension methods

--- bicolorbouncybarview/src/main/java/com/anwesh/uiprojects/bicolorbouncybarview/BiColorBouncyBarView.kt
+++ bicolorbouncybarview/src/main/java/com/anwesh/uiprojects/bicolorbouncybarview/BiColorBouncyBarView.kt
@@ -20,3 +20,9 @@ val foreColor1 : Int = Color.parseColor("#9C27B0")
 val foreColor2 : Int = Color.parseColor("#00695C")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val sizeFactor : Float = 2.9f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+fun Float.cosify() : Float = 1f - Math.sin(Math.PI / 2 + (this * Math.PI * 0.5f)).toFloat()

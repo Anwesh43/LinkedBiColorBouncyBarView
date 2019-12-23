c48073c7c21e0c3ccf8a3b6ef0dbbf8a34959c3c
Anwesh43 <anweshthecool0@gmail.com>
2019-12-23 14:14:11 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbicolorbouncybarview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbicolorbouncybarview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbicolorbouncybarview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bicolorbouncybarview.BiColorBouncyBarView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiColorBouncyBarView.create(this)
     }
 }
